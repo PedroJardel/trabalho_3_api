4ac7eec901b8b0e345078a7220969a23b29a131c
Pedro Lima <89553932+PedroJardel@users.noreply.github.com>
2023-06-21 15:03:34 -0300

    Update conecta.js
    remove pass

--- database/conecta.js
+++ database/conecta.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 
 export const sequelize = new Sequelize (
-    "imoveis", "root", "Lima9800@", {
+    "imoveis", "root", "", {
         dialect: "mysql",
         host: "localhost",
         port: 3306
